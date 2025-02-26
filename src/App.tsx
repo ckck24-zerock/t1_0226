f5a57d31bd318d77c5f12ab620cb11220511787a
zerock <kangckck24@gmail.com>
2025-02-26 15:47:39 +0900

    111

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Count1 from './components/count1'
+
+function App() {
+
+
+
+  return (
+    <>
+      <Count1></Count1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/count1.tsx
@@ -0,0 +1,11 @@
+
+
+function Count1() {
+    return (
+        <div>
+            <h1>Count1</h1>
+        </div>
+    )
+}
+
+export default Count1;
